ee521968b728dc81b087605539a518a151226e6e
MichaelPooleLF <michael.johnpoole@gmail.com>
2021-02-01 20:20:44 -0600

    runs multiple tests

--- codesignal/shapeArea/main.test.js
+++ codesignal/shapeArea/main.test.js
@@ -1,5 +1,9 @@
 const shapeArea = require("./main")
 
-test('returns area of an "n"-interesting polygon', () => {
-  expect(shapeArea(3)).toBe(13);
+test('shapeArea(1) returns 1', () => {
+  expect(shapeArea(1)).toBe(1);
+});
+
+test('shapeArea(2) returns 5', () => {
+  expect(shapeArea(2)).toBe(5);
 });
