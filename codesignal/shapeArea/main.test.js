271e13ae2f473fb67711c64f083985e965742548
MichaelPooleLF <michael.johnpoole@gmail.com>
2021-02-01 20:26:00 -0600

    added check for type of return value

--- codesignal/shapeArea/main.test.js
+++ codesignal/shapeArea/main.test.js
@@ -7,12 +7,15 @@ test('shapeArea(1) returns 1', () => {
 
 test('shapeArea(2) returns 5', () => {
   expect(shapeArea(2)).toBe(5);
+  expect(typeof shapeArea(2)).toBe("number");
 });
 
 test('shapeArea(3) returns 13', () => {
   expect(shapeArea(3)).toBe(13);
+  expect(typeof shapeArea(3)).toBe("number");
 });
 
 test('shapeArea(4) returns 25', () => {
   expect(shapeArea(4)).toBe(25);
+  expect(typeof shapeArea(4)).toBe("number");
 });
