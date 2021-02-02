3ffde5925f7947c84e0900e6eefd874705229fac
MichaelPooleLF <michael.johnpoole@gmail.com>
2021-02-01 20:24:59 -0600

    runs multiple tests

--- codesignal/shapeArea/main.test.js
+++ codesignal/shapeArea/main.test.js
@@ -2,6 +2,7 @@ const shapeArea = require("./main")
 
 test('shapeArea(1) returns 1', () => {
   expect(shapeArea(1)).toBe(1);
+  expect(typeof shapeArea(1)).toBe("number");
 });
 
 test('shapeArea(2) returns 5', () => {
