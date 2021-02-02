e4f5b8197388f1a7b7caaba320705630f34690db
MichaelPooleLF <michael.johnpoole@gmail.com>
2021-02-01 20:21:51 -0600

    runs multiple tests

--- codesignal/shapeArea/main.test.js
+++ codesignal/shapeArea/main.test.js
@@ -7,3 +7,11 @@ test('shapeArea(1) returns 1', () => {
 test('shapeArea(2) returns 5', () => {
   expect(shapeArea(2)).toBe(5);
 });
+
+test('shapeArea(3) returns 13', () => {
+  expect(shapeArea(3)).toBe(13);
+});
+
+test('shapeArea(4) returns 25', () => {
+  expect(shapeArea(4)).toBe(25);
+});
